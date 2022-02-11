273a8f801497180e0470375d2ca47dc569a9c518
Moeda5 <84393810+Moeda5@users.noreply.github.com>
2022-02-11 17:35:41 +0900

    尚硅谷 062

---
+++ 19_单文件组件/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <school></school>
+        <student></student>
+    </div>
+</template>
+
+<script>
+    //App是汇总组件  所要引入子组件
+    import School from "./School.vue"
+    import Student from "./Student.vue"
+
+    export default {
+        name:"App",
+        components:{
+            School,
+            Student
+        }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ 20_DemoCli/ruanfan/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <img src="./assets/logo.png" alt="logo" >
+        <school></school>
+        <student></student>
+    </div>
+</template>
+
+<script>
+    //App是汇总组件  所要引入子组件
+    import School from "./components/School"
+    import Student from "./components/Student"
+
+    export default {
+        name:'App',
+        components:{
+            School,
+            Student
+        }
+    }
+</script>
+
+<style>
+
+</style>

f157ce1babb8efd2df0fe212d9341a96242b93ca
Moeda5 <84393810+Moeda5@users.noreply.github.com>
2022-02-13 23:00:21 +0900

    -070

--- 20_DemoCli/ruanfan/src/App.vue
+++ 20_DemoCli/ruanfan/src/App.vue
@@ -1,28 +1,72 @@
 <template>
-    <div>
-        <!--如果要传int类型 必须加：这样会把引号中的运算结果传走 
-            否则默认传的就是字符串，在接收方不方便做计算
-        -->
-        <school></school>
-        <student></student>
-        <br>
+    
+    <div id="root">
+        
+        
+        
+        <div class="todo-container">
+            <div class="todo-wrap">
+                <top></top>
+                <list></list>
+                <down></down>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-    //App是汇总组件  所要引入子组件
-    import School from "./components/School"
-    import Student from "./components/Student"
+   import Top from './components/Top'
+   import List from './components/List'
+   import Down from './components/Down'
+
 
     export default {
         name:'App',
         components:{
-            School,
-            Student
+           Top,
+           List,
+           Down,
+                
         },
     }
 </script>
 
 <style>
-
+    /*base*/
+    body {
+    background: #fff;
+    }
+    .btn {
+    display: inline-block;
+    padding: 4px 12px;
+    margin-bottom: 0;
+    font-size: 14px;
+    line-height: 20px;
+    text-align: center;
+    vertical-align: middle;
+    cursor: pointer;
+    box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2), 0 1px 2px rgba(0, 0, 0, 0.05);
+    border-radius: 4px;
+    }
+    .btn-danger {
+    color: #fff;
+    background-color: #da4f49;
+    border: 1px solid #bd362f;
+    }
+    .btn-danger:hover {
+    color: #fff;
+    background-color: #bd362f;
+    }
+    .btn:focus {
+    outline: none;
+    }
+    .todo-container {
+    width: 600px;
+    margin: 0 auto;
+    }
+    .todo-container .todo-wrap {
+    padding: 10px;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+    }
 </style>
---
+++ 20_DemoCli/ruanfan/src/components/List.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <ul class="todo-main">
+            <item></item>
+            <item></item>
+            <item></item>
+            <item></item>
+        </ul>
+    </div>
+</template>
+
+<script>
+    import Item from './Item'
+
+    export default {
+    components: { Item },
+        name:'List',
+    }
+</script>
+
+<style scoped>
+    /*main*/
+    .todo-main {
+    margin-left: 0px;
+    border: 1px solid #ddd;
+    border-radius: 2px;
+    padding: 0px;
+    }
+
+    .todo-empty {
+    height: 40px;
+    line-height: 40px;
+    border: 1px solid #ddd;
+    border-radius: 2px;
+    padding-left: 5px;
+    margin-top: 10px;
+    }
+</style>

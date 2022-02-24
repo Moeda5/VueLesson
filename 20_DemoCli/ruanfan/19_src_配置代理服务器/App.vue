55c3b64195884000e815369111fce3ca77a3758d
Moeda5 <84393810+Moeda5@users.noreply.github.com>
2022-02-24 21:57:30 +0900

    -100

---
+++ 20_DemoCli/ruanfan/19_src_配置代理服务器/App.vue
@@ -0,0 +1,43 @@
+<!-- 
+    协议名 主机名 端口号 必须一致 不然会发生跨域问题
+    http  localhost 8080 如果从另外一台服务器拿数据这3个里面其中有东西会不一致
+    解决方法
+    1.cors 这个得后端人员在服务器写 但是一般不这么用 因为写了以后谁都能拿了
+    2.jsonp 这个得前后端一起写 很麻烦 而且只能解决get 不能解决post
+    3.代理 https://cli.vuejs.org/zh/config/#devserver-proxy
+ -->
+<template>
+    <div>
+        <button @click="getStudents">获取学生信息</button>
+        <button @click="getCars">获取学生信息</button>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios'
+    export default {
+        name:'App',
+        methods:{
+            getStudents(){
+                //方法一不需要加前缀
+                // axios.get('http://localhost:8080/students').then(
+                axios.get('http://localhost:8080/ruanfan/students').then(
+                    response => {
+                        console.log('请求成功了',response.data)
+                    },
+                    error => {'请求失败了',error.message}
+                )
+            },
+            getCars(){
+                //方法一不需要加前缀
+                // axios.get('http://localhost:8080/students').then(
+                axios.get('http://localhost:8080/demo/cars').then(
+                    response => {
+                        console.log('请求成功了',response.data)
+                    },
+                    error => {'请求失败了',error.message}
+                )
+            }
+        }
+    }
+</script>
--- 20_DemoCli/ruanfan/vue.config.js
+++ 20_DemoCli/ruanfan/vue.config.js
@@ -7,8 +7,33 @@ module.exports = {
     },
     //关闭语法检查
     lintOnSave:false,
-    //开启代理服务器
+    //开启代理服务器 （方式一）
+    // devServer: {
+    //     proxy: 'http://localhost:5000'
+    //   }
+
+    //开启代理服务器 （方式二） 加/api 就是制定代理服务器走代理而不是本地拿
+    //  这个/api 请求前缀也可以写成别的 只要是这个/xxx  就去/xxx拿
     devServer: {
-        proxy: 'http://localhost:5000'
-      }
+        proxy: {
+        //   '/api': {
+            '/ruanfan': {
+                target: 'http://localhost:5000',
+                //用正则把/ruanfan替换为空字符串再去服务器请求
+                pathRewrite:{'^/ruanfan':''},
+                ws: true,//用于支持websocket
+                changeOrigin: true //请求来自于哪里 true的话服务器会说谎说是5000
+            },
+            // '/foo': {
+            //     target: '<other_url>'
+            // }
+            '/demo': {
+                target: 'http://localhost:5001',
+                //用正则把/ruanfan替换为空字符串再去服务器请求
+                pathRewrite:{'^/demo':''},
+                ws: true,//用于支持websocket
+                changeOrigin: true //请求来自于哪里 true的话服务器会说谎说是5000
+            },
+        }
+    }
 }
